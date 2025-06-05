2c88e6e5aefe4306611da738e8cdb6cfe1c4cd29
Diego34666 <diegoamado.martinez.gmail.com>
2025-06-05 17:01:39 -0400

    add obstacle scene

--- script/player.gd
+++ script/player.gd
@@ -13,3 +13,9 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		apply_central_impulse(Vector2(0,-speed))
 		$AnimatedSprite2D.play("up")
 		
+func game_over():
+	get_tree().paused = true
+	
+func raise_score():
+	score += 1
+	
