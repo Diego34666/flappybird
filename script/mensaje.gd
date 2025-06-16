ecfa9bb33a2f1cde3b9320642452c2296ba2d203
Diego34666 <diegoamado.martinez.gmail.com>
2025-06-16 16:51:49 -0400

    added gui funcionality to the game scene

---
+++ script/mensaje.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+func _input(event: InputEvent) -> void:
+	
+	if Input.is_action_just_pressed("volar") and get_tree().paused:
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+		$".".hide()
+	else:
+		$".".hide()
--- script/player.gd
+++ script/player.gd
@@ -15,7 +15,9 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		
 func game_over():
 	get_tree().paused = true
+	$"../mensaje".show()
 	
 func raise_score():
 	score += 1
+	$"../Label".text = str(score)
 	
